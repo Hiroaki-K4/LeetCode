b53cc5ff0e37bf16b8d67bb9c3a0521c8ad13924
Hiroaki-K4 <49no19@gmail.com>
2021-09-27 22:31:07 +0900

    add isValidSudoku.py

--- isValidSudoku.py
+++ isValidSudoku.py
@@ -18,22 +18,38 @@ class Solution:
 				if board[j][i] in col_dict and board[j][i] != '.':
 					return False
 				col_dict[board[j][i]] = 0
-		return (self.check3_3(board, 0, 0))
+		for i in range(0, 9, 3):
+			for j in range(0, 9, 3):
+				if self.check3_3(board, i, j) == False:
+					return False
+		return True
 
 	def check3_3(self, board, row_start, col_start):
 		check_dict = {}
 		for i in range(row_start, row_start + 3):
 			for j in range(col_start, col_start + 3):
+				# print(i, j)
 				if board[i][j] in check_dict and board[i][j] != '.':
-					print(board[i][j])
+					# print(check_dict)
+					# print(board[i][j])
 					return False
-				check_dict[board[j][i]] = 0
+				check_dict[board[i][j]] = 0
 		return True
 		
 
 if __name__ == '__main__':
 	test = Solution()
 	board = [["5","3",".",".","7",".",".",".","."]
+	,["6",".",".","1","9","5",".",".","."]
+	,[".","9","8",".",".",".",".","6","."]
+	,["8",".",".",".","6",".",".",".","3"]
+	,["4",".",".","8",".","3",".",".","1"]
+	,["7",".",".",".","2",".",".",".","6"]
+	,[".","6",".",".",".",".","2","8","."]
+	,[".",".",".","4","1","9",".",".","5"]
+	,[".",".",".",".","8",".",".","7","9"]]
+	print(test.isValidSudoku(board))
+	board = [["8","3",".",".","7",".",".",".","."]
 	,["6",".",".","1","9","5",".",".","."]
 	,[".","9","8",".",".",".",".","6","."]
 	,["8",".",".",".","6",".",".",".","3"]
