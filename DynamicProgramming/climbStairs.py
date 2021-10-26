823e63c3b6360ea878279267da76079e17fe7c54
Hiroaki-K4 <49no19@gmail.com>
2021-10-26 11:41:12 +0900

    add climbStairs.py

---
+++ DynamicProgramming/climbStairs.py
@@ -0,0 +1,18 @@
+
+
+class Solution:
+	def climbStairs(self, n: int) -> int:
+		dp = [1] * (n+1)
+		for i in range(2, n+1):
+			dp[i] = dp[i-1] + dp[i-2]
+		return dp[n]
+
+
+if __name__ == '__main__':
+	test = Solution()
+	n = 2
+	print(test.climbStairs(n))
+	n = 3
+	print(test.climbStairs(n))
+	n = 4
+	print(test.climbStairs(n))
